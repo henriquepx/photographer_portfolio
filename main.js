771bf57d6e02047ddfc497b0ee15a3e0a1589e40
Henrique <henriquepinheiroxavier@gmail.com>
2023-10-05 10:47:33 -0300

    feat: navegation tabs and create all sections files

--- main.js
+++ main.js
@@ -2,3 +2,19 @@ $('.image-container').hover(function() {
   $('.image-container').toggleClass('shadow');
   $(this).toggleClass('active shadow');
 });
+
+const tabMenu = document.querySelectorAll('.jstabmenu');
+const tabContent = document.querySelectorAll('.jstabcontent');
+
+function activeTab(index) {
+  tabContent.forEach((section) => {
+    section.classList.remove('ativo');
+  });
+  tabContent[index].classList.add('ativo');
+}
+
+tabMenu.forEach((itemMenu, index) => {
+  itemMenu.addEventListener('click', () => {
+    activeTab(index);
+  })
+})
