87543d07fd03d5d68d36c82f08d0606456b03914
Henrique <henriquepinheiroxavier@gmail.com>
2023-10-13 10:17:23 -0300

    feat: add description under title

--- main.js
+++ main.js
@@ -30,3 +30,45 @@ document.querySelectorAll(".nav-link").forEach((n) =>
     navMenu.classList.remove("active");
   })
 ); 
+
+// TEXT WRITING
+
+const greeting = ['Photographer', 'Filmmaker'];
+let currentGreetingIndex = 0;
+let currentCharacterIndex = 0;
+let isDeleting = false;
+let isPaused = false;
+let pauseEnd = 0;
+
+function typeWriterEffect() {
+  const greetingElement = document.getElementById('typing');
+
+  if (isPaused && Date.now() > pauseEnd) {
+    isPaused = false;
+    if (isDeleting) {
+      currentGreetingIndex = (currentGreetingIndex + 1) % greeting.length;
+      isDeleting = false;
+    } else {
+      isDeleting = true;
+    }
+  }
+
+  if (!isPaused && !isDeleting && currentCharacterIndex === greeting[currentGreetingIndex].length) {
+    isPaused = true;
+    pauseEnd = Date.now() + 800; 
+    return setTimeout(typeWriterEffect, 40);
+  }
+
+  if (!isPaused && isDeleting && currentCharacterIndex === 0) {
+    isPaused = true;
+    pauseEnd = Date.now() + 200; 
+    return setTimeout(typeWriterEffect, 40);
+  }
+
+  const timeout = isDeleting ? 100 : 200;
+  greetingElement.innerText = greeting[currentGreetingIndex].substring(0, currentCharacterIndex);
+  currentCharacterIndex = isDeleting ? currentCharacterIndex - 1 : currentCharacterIndex + 1;
+  setTimeout(typeWriterEffect, timeout);
+}
+
+typeWriterEffect();
