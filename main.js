e4829cf0ea3cc33d9bb13acc782596907aa36ebe
Henrique <henriquepinheiroxavier@gmail.com>
2023-10-12 23:09:31 -0300

    feat: responsible menu hamburger

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// TAB NAVEGATION
 const tabMenu = document.querySelectorAll('.jstabmenu');
 const tabContent = document.querySelectorAll('.jstabcontent');
 
@@ -14,3 +15,18 @@ tabMenu.forEach((itemMenu, index) => {
   })
 })
 
+// MENU HAMBURGER 
+const hamburger = document.querySelector(".menuhamburger");
+const navMenu = document.querySelector(".menu");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
+
+document.querySelectorAll(".nav-link").forEach((n) =>
+  n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  })
+); 
