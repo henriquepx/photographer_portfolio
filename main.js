66ae6ed90c3fa2f583b15c337c3780ef7c1cc199
Henrique <henriquepinheiroxavier@gmail.com>
2023-10-17 19:37:41 -0300

    feat: add new bg main

--- main.js
+++ main.js
@@ -18,3 +18,25 @@ MenuButton.addEventListener('click', () => {
         MoreLess.textContent = '+';
     }
 });
+
+function slidesPlugin(activeSlide = 2) {
+	const slides = document.querySelectorAll(".slide");
+
+	slides[activeSlide].classList.add("active");
+
+	for (const slide of slides) {
+		slide.addEventListener("click", () => {
+			clearActiveClasses();
+
+			slide.classList.add("active");
+		});
+	}
+
+	function clearActiveClasses() {
+		slides.forEach((slide) => {
+			slide.classList.remove("active");
+		});
+	}
+}
+
+slidesPlugin();
